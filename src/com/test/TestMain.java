bc649cab5ad7385e60fffde34cbdeee6be56f661
xu.sl <xu.sl@Ctrip.com>
2014-12-31 15:40:17 +0800

    Test Reset

--- src/com/test/TestMain.java
+++ src/com/test/TestMain.java
@@ -1,10 +1,9 @@
 package com.test;
 
-<<<<<<< HEAD
+
 import com.test.xsl.SayHellp;
 
-=======
->>>>>>> a272cdc6ed283bbe469e2b856a270e610e8bcb2b
+
 public class TestMain {
 
 	public static void main(String[] args) {
@@ -12,10 +11,9 @@ public class TestMain {
 		System.out.println("My Study");
 		System.out.println("╪ссм");
 		TestLog.print();
-<<<<<<< HEAD
+
 		SayHellp.t();
-=======
->>>>>>> a272cdc6ed283bbe469e2b856a270e610e8bcb2b
+
 
 	}
 
